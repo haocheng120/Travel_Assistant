24c17ceaac9933479e006de2639678046f264347
haocheng120 <a0981887608@gmail.com>
2026-04-02 19:11:11 +0800

    Initial commit

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'korea-trip-v1';
+const ASSETS_TO_CACHE = [
+  './seoul-travel.html',
+  './manifest.json',
+  './static/img/korea.png'
+];
+
+// 安裝階段：快取靜態資源
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// 攔截請求：如果沒網路，就從快取拿資料
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // 如果快取裡有，就回傳快取；沒有就透過網路抓
+      return response || fetch(event.request);
+    })
+  );
+});
